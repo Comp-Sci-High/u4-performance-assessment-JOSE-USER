1e6083df1f189a54dd4e60ddf3f49f9337565766
JOSE-USER <jose.corniel25@compscihigh.org>
2025-02-04 20:12:32 +0000

    2/3 complete

--- index.js
+++ index.js
@@ -3,29 +3,29 @@ const app = express()
 
 app.set("view-engine","ejs")
 
-app.use(express.static(index.js__dirname + "/public"))
-
 app.use((req,res,next)=>{
     res.status(404).send("404 NOT FOUND")
 })
 
-app.get("/home",(req,res)=>{
-    res.render
+
+app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + "/views"))
+
+app.get("/",(req,res)=>{
+  res.sendFile(__dirname + "/public/home.html")
 })
 
 
 
+app.get("/overview", (req,res)=>{
+  const data = {GroupSize: 3 , Title: "Mini Igloo" ,
+    Difficultytobuild: 6,
+    VideoLink: "https://www.youtube.com/watch?v=Ulem2zFaK2E&pp=ygUOdHJpbyBydXN0IGJhc2U%3D" ,}
+    res.render("overview.ejs", data)
+})
 
 
 
-
-const groupBases = {
-    Group-Size: ,
-    Title: ,
-    Difficulty-to-build:,
-    Video-Link: ,
-   }
-   
 
    app.use((req, res, next) => {
     console.log(req.method + " " + req.path)
